250be7b49f07d9d5a765873bef85f9dccbcc4e5c
kamal kishore <kamal.kishore981@gmail.com>
2022-09-13 16:00:24 -0400

    vaildate compared the hash pass

--- models/User.js
+++ models/User.js
@@ -5,7 +5,14 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 // create our User model
 
-class User extends Model{}
+class User extends Model{
+// set up method to run on instance data (per user) to check password 
+ checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+
+
+}
 
 
 
@@ -78,9 +85,12 @@ User.init(
     underscored: true,
     // make it so our model name stays lowercase in the database
     modelName: `user`,
-  }
+  },
+
+  
 );
 
+// create our User model
 
 
 
