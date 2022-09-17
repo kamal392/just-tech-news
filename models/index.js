abf307ebcd806f58540664fbd0978c872946c945
kamal kishore <kamal.kishore981@gmail.com>
2022-09-17 16:59:04 -0400

    post model created

--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
-const User = require('./User');
 
-module.exports = {User};
+// // Importing User model so that we can use in the other part of the application
+// const User = require('./User');
+
+// // importing Post model so that we can export it to use to the other part of the application
+
+// const Post = require("./Post");
+
+// // exporting User and post models to use in the other part of the application.
+// module.exports = {User,Post};
+
+const User = require("./User");
+const Post = require("./Post");
+
+module.exports = { User, Post };
