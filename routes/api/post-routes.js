5ca960f0a4a58594e412673a528a42a1790a99c9
kamal kishore <kamal.kishore981@gmail.com>
2022-09-21 02:06:18 -0400

    updates routs

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,9 +1,8 @@
 // create express.js API endpoints
 const router = require('express').Router();
-
-const{Post,User} = require('../../models');
+const sequelize = require('../../config/connection');
+const{Post,User,Vote} = require('../../models');
 const { get, post } = require('./user-routes');
-// const { router} = require('./user-routes');
 
 // get all users
 
@@ -12,7 +11,15 @@ console.log('======================');
 Post.findAll({
     // Query configuration
     // customize the attributes property
-attributes:['id','post_url','title','created_at'],
+attributes:[
+'id',
+'post_url',
+'title',
+'created_at',
+[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    
+],
+
 order:[['created_at','DESC']],
 include:[
   {
@@ -39,7 +46,13 @@ include:[
    where:{
     id:req.params.id
    },
-  attributes:['id','post_url','title','created_at'],
+  attributes:['id',
+  'post_url',
+  'title',
+  'created_at',
+  [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    
+],
 
   include:[
     {
@@ -82,7 +95,17 @@ router.post('/',(req,res)=>{
 });
 });
 
-// create route to update the Post title
+
+router.put("/upvote", (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
 
 router.put('/:id',(req,res)=>{
 
@@ -114,23 +137,19 @@ router.put('/:id',(req,res)=>{
 router.delete("/:id", (req, res) => {
   Post.destroy({
     where: {
-      id: req.params.id,
-    },
+      id: req.params.id
+    }
   })
-    .then((dbPostData) => {
+    .then(dbPostData => {
       if (!dbPostData) {
         res.status(404).json({ message: "No post found with this id" });
         return;
       }
       res.json(dbPostData);
     })
-    .catch((err) => {
+    .catch(err=> {
       console.log(err);
       res.status(500).json(err);
     });
 });
-
-
-
-
 module.exports = router;
